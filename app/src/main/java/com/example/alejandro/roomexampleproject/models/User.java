a9eb19e01d379e552ce25f8958271010535dea06
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-05-14 01:53:23 -0500

    created packages, migrated MainActivity file to its corresponding package, created model User with its corresponding Annotations

---
+++ app/src/main/java/com/example/alejandro/roomexampleproject/models/User.java
@@ -0,0 +1,52 @@
+package com.example.alejandro.roomexampleproject.models;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity
+public class User {
+    @PrimaryKey
+    private int id;
+
+    @ColumnInfo(name = "first_name")
+    private String name;
+
+    @ColumnInfo(name = "last_name")
+    private String lastName;
+
+    @ColumnInfo(name = "phone_number")
+    private String phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
