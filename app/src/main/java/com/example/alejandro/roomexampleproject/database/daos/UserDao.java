6775ab0f5c7ae6cfa00e0b2574b9712971a168d6
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-05-14 02:08:23 -0500

    added dao for User model

---
+++ app/src/main/java/com/example/alejandro/roomexampleproject/database/daos/UserDao.java
@@ -0,0 +1,32 @@
+package com.example.alejandro.roomexampleproject.database.daos;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import com.example.alejandro.roomexampleproject.models.User;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+
+    @Query("SELECT * FROM user")
+    List<User> getAll();
+
+    @Query("SELECT * FROM user WHERE id IN (:userIds)")
+    List<User> loadAllByIds(int[] userIds);
+
+    @Query("SELECT * FROM user WHERE first_name LIKE (:name) LIMIT 1")
+    User findByName(String name);
+
+    @Query("SELECT * FROM user WHERE first_name LIKE (:first) AND last_name LIKE (:last) LIMIT 1")
+    User findByFullName(String first, String last);
+
+    @Insert
+    void insertAll(User... users);
+
+    @Delete
+    void delete(User user);
+}
