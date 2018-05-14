84605ad35341abcd9be244414a934b05489d67c8
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-05-14 03:43:56 -0500

    added Singleton implementation of Room Database holder component

--- app/src/main/java/com/example/alejandro/roomexampleproject/database/AppDatabase.java
+++ app/src/main/java/com/example/alejandro/roomexampleproject/database/AppDatabase.java
@@ -1,12 +1,37 @@
 package com.example.alejandro.roomexampleproject.database;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
+import com.example.alejandro.roomexampleproject.database.daos.NoteDao;
 import com.example.alejandro.roomexampleproject.database.daos.UserDao;
+import com.example.alejandro.roomexampleproject.models.Note;
 import com.example.alejandro.roomexampleproject.models.User;
 
-@Database(entities = {User.class}, version = 1)
+@Database(entities = {User.class, Note.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase{
+    private static final String DB_NAME = "notesDatabase.db";
+    private static volatile AppDatabase instance;
+
+    static synchronized AppDatabase getInstance(Context context){
+        if (instance == null){
+            instance = create(context);
+        }
+
+        return instance;
+    }
+
+    private static AppDatabase create(Context context) {
+        return Room.databaseBuilder(
+                context,
+                AppDatabase.class,
+                DB_NAME
+        ).build();
+    }
+
     public abstract UserDao userDao();
+    public abstract NoteDao noteDao();
+
 }
