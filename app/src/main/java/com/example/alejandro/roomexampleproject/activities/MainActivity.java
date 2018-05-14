f07ec48c89098ac0c01922e92488108f2157dfae
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-05-14 02:10:31 -0500

    files modified for the migration of MainActivity to corresponding package

--- app/src/main/java/com/example/alejandro/roomexampleproject/activities/MainActivity.java
+++ app/src/main/java/com/example/alejandro/roomexampleproject/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.alejandro.roomexampleproject;
+package com.example.alejandro.roomexampleproject.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(com.example.alejandro.roomexampleproject.R.layout.activity_main);
     }
 }
