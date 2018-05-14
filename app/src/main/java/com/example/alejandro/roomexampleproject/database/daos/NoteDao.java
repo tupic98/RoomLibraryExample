9001c79848d73a13d84a11ad39ac5239c2a7d022
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-05-14 03:31:40 -0500

    added Note DAO

---
+++ app/src/main/java/com/example/alejandro/roomexampleproject/database/daos/NoteDao.java
@@ -0,0 +1,32 @@
+package com.example.alejandro.roomexampleproject.database.daos;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.example.alejandro.roomexampleproject.models.Note;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+    @Insert
+    void insert(Note note);
+
+    @Update
+    void update(Note... notes);
+
+    @Delete
+    void delete(Note... notes);
+
+    @Query("SELECT * FROM Note")
+    List<Note> getAll();
+
+    @Query("SELECT * FROM Note where id=(:id)")
+    Note findNoteById(int id);
+
+    @Query("SELECT * FROM Note where user_id=(:userId)")
+    List<Note> getAllNotesByUser(int userId);
+}
