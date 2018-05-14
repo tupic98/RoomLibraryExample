28b09060f48506112da3e30a002104d7d055e4f0
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-05-14 03:04:41 -0500

    added Note model with its annotations

---
+++ app/src/main/java/com/example/alejandro/roomexampleproject/models/Note.java
@@ -0,0 +1,45 @@
+package com.example.alejandro.roomexampleproject.models;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(foreignKeys = @ForeignKey(entity = User.class,
+                                            parentColumns = "id",
+                                            childColumns = "user_id",
+                                            onDelete = ForeignKey.CASCADE))
+public class Note {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "data")
+    private String data;
+
+    @ColumnInfo(name = "user_id")
+    private int userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
