5310b9c3812bb421ff4d8923dd9b26a9c707aef9
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-05-14 05:03:43 -0500

    added drawernavigation implementation

--- app/src/main/java/com/example/alejandro/roomexampleproject/activities/MainActivity.java
+++ app/src/main/java/com/example/alejandro/roomexampleproject/activities/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.alejandro.roomexampleproject.activities;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.example.alejandro.roomexampleproject.R;
+import com.example.alejandro.roomexampleproject.fragments.UserInfoFragment;
+import com.example.alejandro.roomexampleproject.models.User;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.example.alejandro.roomexampleproject.R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                drawerLayout.closeDrawers();
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                UserInfoFragment userInfoFragment = new UserInfoFragment();
+                userInfoFragment.setUser(new User());
+                fragmentTransaction.replace(R.id.contentFrame, userInfoFragment);
+                fragmentTransaction.commit();
+                return true;
+            }
+        });
+
     }
 }
