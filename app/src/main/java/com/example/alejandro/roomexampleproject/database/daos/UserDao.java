963aa603e7fb4f93d445321f1109ea875ff6c67a
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-05-14 03:21:16 -0500

    added new methods for UserDAO

--- app/src/main/java/com/example/alejandro/roomexampleproject/database/daos/UserDao.java
+++ app/src/main/java/com/example/alejandro/roomexampleproject/database/daos/UserDao.java
@@ -4,6 +4,7 @@ import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 
 import com.example.alejandro.roomexampleproject.models.User;
 
@@ -21,6 +22,9 @@ public interface UserDao {
     @Query("SELECT * FROM user WHERE first_name LIKE (:name) LIMIT 1")
     User findByName(String name);
 
+    @Query("SELECT * FROM user WHERE last_name LIKE (:lastName) LIMIT 1")
+    User findByLastName(String lastName);
+
     @Query("SELECT * FROM user WHERE first_name LIKE (:first) AND last_name LIKE (:last) LIMIT 1")
     User findByFullName(String first, String last);
 
@@ -29,4 +33,7 @@ public interface UserDao {
 
     @Delete
     void delete(User user);
+
+    @Update
+    void updateUsers(User... users);
 }
