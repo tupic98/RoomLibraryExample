1853bf12d0cd902d9fb00da620db3a0956ace139
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-05-14 04:57:33 -0500

    added UserInfoFragment class

---
+++ app/src/main/java/com/example/alejandro/roomexampleproject/fragments/UserInfoFragment.java
@@ -0,0 +1,38 @@
+package com.example.alejandro.roomexampleproject.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.alejandro.roomexampleproject.R;
+import com.example.alejandro.roomexampleproject.models.User;
+
+public class UserInfoFragment extends Fragment{
+    User user;
+    TextView firstName, lastName;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.user_info, container, false);
+
+        firstName = v.findViewById(R.id.firstName);
+        lastName = v.findViewById(R.id.lastName);
+
+        firstName.setText(user.getFirstName());
+        lastName.setText(user.getLastName());
+
+        return v;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
