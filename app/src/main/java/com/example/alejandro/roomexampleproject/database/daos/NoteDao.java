b3d10f2e96b2feca151f19920870dc18a4da5b16
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-05-14 06:06:53 -0500

    added onconflict attribute to insert method of dao

--- app/src/main/java/com/example/alejandro/roomexampleproject/database/daos/NoteDao.java
+++ app/src/main/java/com/example/alejandro/roomexampleproject/database/daos/NoteDao.java
@@ -3,6 +3,7 @@ package com.example.alejandro.roomexampleproject.database.daos;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
@@ -13,7 +14,7 @@ import java.util.List;
 
 @Dao
 public interface NoteDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Note note);
 
     @Update
